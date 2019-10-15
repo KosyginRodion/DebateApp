4be52e377a3b6af154a1ee8c730547e08be32807
Sokolovskii Alexander <sokolovskii.alex2@gmail.com>
2019-10-15 21:09:31 +1000

    Допилил добавление человека пока в виде кучи переменных

---
+++ Source/DebateApp.Mvc/Controllers/PersonController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DebateApp.DataAccess.Repository;
+using DebateApp.Models;
+using DebateApp.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DebateApp.Mvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+			var persons = GetPersons();
+
+            return View(persons);
+        }
+
+		private IEnumerable<Person> GetPersons()
+		{
+			var persons = new List<Person>();
+			persons.Add(new Person
+			{
+				FirstName = "Рома",
+				LastName = "Зоренко",
+				Id = 3,
+				Email = "wkbwkefbqwfhqwojf"
+			});
+			persons.Add(new Person
+			{
+				FirstName = "Родион",
+				LastName = "Косыгин",
+				Id = 1,
+				Email = "тулатутцрацпца"
+			});
+			persons.Add(new Person
+			{
+				FirstName = "Саша",
+				LastName = "Соколовский",
+				Id = 2,
+				Email = "уацацуафыасфаы"
+			});
+
+			return persons.OrderBy(p => p.Id);
+		}
+
+		// GET: Person/Create
+		public ActionResult Create(string FirstName, string LastName, string Email)
+        {
+			Person person = new Person();
+			person.FirstName = FirstName;
+			person.LastName = LastName;
+			person.Email = Email;
+
+			//PersonService personService = new PersonService();
+			//personService.Add(person);
+
+            return View();
+        }
+    }
+}
